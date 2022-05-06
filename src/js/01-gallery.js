510beac916d6473642cdcd42a737b237edb708d1
AnastasiiaSukha <suha.anastasia@gmail.com>
2022-05-06 18:34:26 +0400

    1st task hw08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+
+const gallery = document.querySelector("div.gallery");
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', itemsMarkup);
+
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        
+        
+        return `
+        <div class="gallery__item">
+  <a class="gallery__item"
+  href="${original}">
+  <img class="gallery__image"
+  src="${preview}"
+  alt="${description}"
+  />
+</a>
+</div>
+    `
+    }).join('');
+    
+};
+
+new SimpleLightbox('.gallery a', {captionsData:'alt', captionDelay:250});
+
