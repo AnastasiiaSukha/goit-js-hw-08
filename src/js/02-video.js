43a33b1a84c38541ba29824febefa43a800e351a
AnastasiiaSukha <suha.anastasia@gmail.com>
2022-05-06 19:56:01 +0400

    task 01 and half 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
 
+// import Player from '@vimeo/player';
+
+
+
+// const player = new Vimeo.Player('vimeo-player');
+
+// const onPlay = function (data) {
+    
+// }
+
+// player.on('timeupdate', onPlay)
