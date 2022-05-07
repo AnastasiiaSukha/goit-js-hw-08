0093aec7abeebd35c3243c1703c715817d2439c6
AnastasiiaSukha <suha.anastasia@gmail.com>
2022-05-08 00:34:35 +0300

    update video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,30 @@
 
-// import Player from '@vimeo/player';
+import Player from '@vimeo/player';
 
 
+const iframe = document.querySelector('iframe');
 
-// const player = new Vimeo.Player('vimeo-player');
-
-// const onPlay = function (data) {
+const player = new Vimeo.Player(iframe);
     
-// }
 
-// player.on('timeupdate', onPlay)
+player.on('timeupdate', function (data) {
+    
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+});
+
+const loadData = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+
+const currentSeconds = loadData.seconds;
+
+player.setCurrentTime(currentSeconds).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
+    
+
