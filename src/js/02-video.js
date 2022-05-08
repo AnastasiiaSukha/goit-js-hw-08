05b2e15cb23942b09c99bdf997f91ac58051d144
AnastasiiaSukha <suha.anastasia@gmail.com>
2022-05-08 11:14:32 +0300

    HW ts 2 vimeo done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,20 @@
 
 import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
 
 const iframe = document.querySelector('iframe');
 
 const player = new Vimeo.Player(iframe);
-    
 
-player.on('timeupdate', function (data) {
-    
+
+
+
+function onTimeSave(data) {
     localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
-});
+};
+    
+player.on('timeupdate', throttle(onTimeSave,1000));
 
 const loadData = JSON.parse(localStorage.getItem("videoplayer-current-time"));
 
