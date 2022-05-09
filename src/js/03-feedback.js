0d6e4979e5f0267df6bd17a5d88036f283525093
AnastasiiaSukha <suha.anastasia@gmail.com>
2022-05-09 11:21:15 +0400

    HW 08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
 
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const text = document.querySelector('textarea');
+const submit = document.querySelector('button');
+
+form.addEventListener('submit', OnFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+
+updateForm();
+
+function OnFormSubmit(event) {
+    event.preventDefault();
+    event.target.reset();
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    localStorage.removeItem(STORAGE_KEY);
+
+};
+
+function onFormInput(event) {
+    const email = input.value;
+    const message = text.value;
+
+    const data = {
+        email: email,
+        message: message,
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+
+};
+
+
+
+
+function updateForm() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedData) {
+        
+        input.value = savedData.email;
+        text.value = savedData.message;
+
+    } 
+    
+};
+
